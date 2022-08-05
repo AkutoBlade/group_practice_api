79e9e63def5f0f252393dc854c9b05725bf80d6b
AkutoBlade <rared.isaacs@gmail.com>
2022-08-05 09:24:13 +0200

    Update

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ router.get('/login', (req, res)=> {
 app.post('/register',bodyParser.json(), 
     async (req, res)=> {
     try{
-    const submit =document.querySelector('submit');
         const bd = req.body; 
         if(bd.userRole === ' ' || bd.userRole === null) {
             bd.userRole = 'user';
@@ -61,11 +60,6 @@ app.post('/register',bodyParser.json(),
                 if(err) throw err;
                 console.log(results)
             })
-    //   submit.addEventListener('submit',()=>{
-    //     router.get('/login', (req, res)=> {
-    //     res.status(200).sendFile(path.join(__dirname, 'views', 'login.html'));
-    //     });
-    //   })
     }catch(e) {
         console.log(`From registration: ${e.message}`);
     }
